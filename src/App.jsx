da5228fd3a88844915b7f9ce0567bb8f1bd3501f
faatima30 <faatimaapdirahmaanali@gmail.com>
2026-02-25 08:11:57 +0300

    first assignment

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import UserCard from './components/UserCard '
+function App() {
+
+  return (
+    <>
+      
+      <h1>here is the assignment</h1>
+      <UserCard/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/UserCard .jsx
@@ -0,0 +1,16 @@
+
+function UserCard () {
+
+  return (
+    <>
+      
+      <h2>fatima</h2>
+      <p>
+fatima@gmail.com
+      </p>
+      
+    </>
+  )
+}
+
+export default UserCard 
